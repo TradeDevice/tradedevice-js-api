9c2f4d028cc1cd9e6eb505954b31b589f90746fb
Matthew Gross <mattgross1@ymail.com>
2013-07-08 12:11:31 -0400

    Adding - NOT FINISHED YET

---
+++ src/TradeDevice.js
@@ -0,0 +1,34 @@
+// TradeDevice Javascript API Library
+// License: MIT
+// TradeDevice.com
+
+var api_root;
+var country;
+
+var url = "tradedevice.com/api/";
+
+function TradeDevice(country) {
+	if(country === "US") country = "us";
+	if(country === "CA") country = "ca";
+	this.country = country;
+}
+
+TradeDevice.prototype.getApiRoot = function()
+{
+	country = this.country;
+	root = country + this.url;
+	return root
+});
+
+TradeDevice.prototype.getData = function(query)
+{
+	data;
+	root = this.getApiRoot();
+	fin = root + query;
+	json = new XMLHttpRequest();
+	json.open('GET', fin, true);
+	json.onload = function() {
+		data = window.JSON.parse(json);
+	};
+	return data;
+});
